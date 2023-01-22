54e9aed6068dbaa5b398d819b873995fad7e5006
Dominik Gabinski <gabinski.dominik@gmail.com>
2023-01-22 12:10:58 +0100

    Add Lista_zakopow.py

---
+++ Lista_zakopow.py
@@ -0,0 +1,9 @@
+Lista_zakopow = {
+    "piekarnia" : ["chleb", "pączek", "bułki"],
+    "warzywniak" : ["marchew", "seler", "rukola"]
+}
+
+for k, v in Lista_zakopow.items():
+    print("Idę do", k.capitalize(), "i kupuję tam", v[0].title(),v[1].title(),v[2].title() )
+    
+print("W sumie kupuję", sum(len(v) for v in Lista_zakopow.values()), "produktów")
