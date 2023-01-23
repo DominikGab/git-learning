88689679e46084b43e37178ab46a711ff0c8a913
DominikGab <123317747+DominikGab@users.noreply.github.com>
2023-01-23 20:37:25 +0100

    Fix sketch info

--- Lista_zakopow.py
+++ Lista_zakopow.py
@@ -7,3 +7,5 @@ for k, v in Lista_zakopow.items():
     print("Idę do", k.capitalize(), "i kupuję tam", v[0].title(),v[1].title(),v[2].title() )
     
 print("W sumie kupuję", sum(len(v) for v in Lista_zakopow.values()), "produktów")
+
+print("Hiszpańska inkwizycja to najlepszy skecz")
